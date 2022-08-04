9593b3967fdb36cbf97f9ec7cb781df3101e7de1
leotang40 <leotang40@gmail.com>
2022-08-04 20:05:27 +0800

    modify posts/create.php

--- summer_preject_eric/app/Views/posts/create.php
+++ summer_preject_eric/app/Views/posts/create.php
@@ -4,35 +4,92 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>大學徵選委員會網頁-大學繁星推薦</title>
+  <title>大學徵選委員會網頁-公告管理頁面</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-  <header class="container-fluid text-center" style="border: 1px solid black; height: 100px">
-    <h1>大學繁星推薦</h1>
-    <h2>發公告網頁測試</h2>
-  </header>
-  <main class="container-fluid text-center">
+  <header class="container-fluid text-center" style="border: 1px solid black">
     <div class="row">
-      <div class="col-2" style="border: 1px solid black">
-        <h5>側邊標題欄</h5>
-        <h4>側邊標題欄</h4>
-        <h3>側邊標題欄</h3>
-        <h3>側邊標題欄</h3>
-        <h3>側邊標題欄</h3>
-        <h3>側邊標題欄</h3>
+      <div class="col-8 text-start">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">回首頁</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">大學繁星</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">大學個申</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">高中繁星</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">高中個申</a>
+          </li>
+        </ul>
       </div>
-      <div class="col-10" style="border: 1px solid black">
+      <div class="col-4 text-end">
+        <!-- if user not login -->
+        <span>未登入</span>
+        <a href="#"><button type="button" class="btn btn-light">管理者登入</button></a>
+        <!-- if user login -->
+        <!-- <span>使用者</span> -->
+        <!-- <a href="#"><button type="button" class="btn btn-light">登出</button></a> -->
+      </div>
+      <h1>公告管理頁面</h1>
+    </div>
+  </header>
+  <main class="container-fluid text-center" style="height: 70vh">
+    <div class="row">
+      <div class="col-md-2" style="border: 1px solid black">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="#">已發布公告</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">草稿</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">發布公告</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">控制頁面</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-10" style="border: 1px solid black">
+        <h3>發布公告</h3>
         <form action="/PostController/store" enctype="mutipart/form-data" method="POST">
-          <label for="title">標題<input type="text" name="title"></label><br>
-          <label for="content">內文<input type="text" name="content"></label><br>
+          <label for="title">標題<input type="text" name="title" required></label><br>
+          <label for="website">發布到哪個網頁
+            <select name="website">
+              <option value="1">大學繁星</option>
+              <option value="2">大學個申</option>
+              <option value="3">高中繁星</option>
+              <option value="4">高中個申</option>
+            </select>
+          </label><br>
+          <label for="category">分類
+            <select name="category">
+              <option value="1">簡章訊息事項</option>
+              <option value="2">招生事務</option>
+              <option value="3">徵選資訊</option>
+              <option value="4">會議簡報</option>
+              <option value="5">其他事項</option>
+            </select>
+          </label><br>
+          <label for="content">內文<textarea name="content" required></textarea></label><br>
+          <label for="file">上傳檔案<input type="file" name="file"></label><br>
+          <label for="dateStart">公告發布時間<input id="dateStart" type="date" name="dateStart" required></label><br>
+          <label for="dateEnd">公告下架時間<input id="dateEnd" type="date" name="dateEnd" required></label><br>
           <button type="submit">Submit</button>
         </form>
       </div>
     </div>
   </main>
-  <footer class="container-fluid text-center" style="border: 1px solid black; height: 100px">
-    <h3>footer</h3>
+  <footer class="container-fluid text-center" style="border: 1px solid black">
+    <h4>聯絡資訊</h4> 
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
