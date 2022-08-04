e65233b89f56c839167447a288f66c7068b0229e
leotang40 <leotang40@gmail.com>
2022-08-04 16:39:58 +0800

    universityStar/index layout test

--- summer_preject_eric/app/Controllers/PostController.php
+++ summer_preject_eric/app/Controllers/PostController.php
@@ -10,4 +10,14 @@ class PostController extends BaseController
     {
         return view('universityStar/index');
     }
+
+    public function store()
+    {
+        $data = [
+            'title' => $this->request->getVar('title'),
+            'content' => $this->request->getVar('content')
+        ];
+
+        print_r($data);
+    }
 }
