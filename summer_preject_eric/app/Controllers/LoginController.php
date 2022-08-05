f5f1e4fa9a93853c05ca514664d0b97d944bbac0
eric_git <ericier11222255@gmail.com>
2022-08-05 19:18:09 +0800

    mail_test

--- summer_preject_eric/app/Controllers/LoginController.php
+++ summer_preject_eric/app/Controllers/LoginController.php
@@ -4,12 +4,11 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\Login;
-
+session_start();
 class LoginController extends BaseController
 {
     public function index()
     {
-        session_start();
         if(!isset($_SESSION['LOGIN'])){
             $_SESSION['LOGIN'] = 0;
             }
@@ -17,32 +16,52 @@ class LoginController extends BaseController
     }
     public function check()
     {
+        $message = "hello";
+        $email = \Config\Services::email();
+        $email->setFrom('twbooster@yahoo.com', 'from CI4');
+        $email->setTo('twimposter@yahoo.com');
+        $email->setSubject('hi');
+        $email->setMessage($message);//your message here
+        
+    
+        $email->send();
+        
+
+        // $this->load->library('email');
+        // $this->email->from('twbooster@yahoo.com', 'Your Name');
+        // $this->email->to('ericier11222255@gmail.com');
+        // $this->email->subject('Email Test');
+        // $this->email->message('Testing the email class.');
+
+        // $this->email->send();
         $model = new Login();
         
         $logins = [                         //抓全部資料
             'logins' => $model->findAll()
         ]; 
         foreach($logins as $logins_item){
-            print_r($logins_item);
+            foreach($logins_item as $item){
+                print_r($item['account']);
+            }
         }
 
-        session_start();
-        if(isset($_SESSION['LOGIN'])){
-            if(!empty($logins)) {
+        // session_start();
+        // if(isset($_SESSION['LOGIN'])){
+        //     if(!empty($logins)) {
                 
-            }
-            if($_POST['account'] == 'test'){
-                $_SESSION['LOGIN'] = 1;
-                return redirect('Home');
-                }
-            else{
-                echo '<script>alert("incorrect username or password")</script>';
-                return view('logins/index');
-            }
-        }
-        else{
-        return redirect('Home');
-        }
+        //     }
+        //     if($_POST['account'] == 'test'){
+        //         $_SESSION['LOGIN'] = 1;
+        //         return redirect('Home');
+        //         }
+        //     else{
+        //         echo '<script>alert("incorrect username or password")</script>';
+        //         return view('logins/index');
+        //     }
+        // }
+        // else{
+        // return redirect('Home');
+        // }
         $data = [
             'account' => $this->request->getVar('account'), 
             'password' => $this->request->getVar('password')
