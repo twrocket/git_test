e8dfcc6f1fedeb566b85e2cbe0e175de245e70e6
leotang40 <leotang40@gmail.com>
2022-08-25 17:15:15 +0800

    add highApply Web

---
+++ summer_preject_eric/app/Controllers/HighApply.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\Post;
+
+class HighApply extends BaseController
+{
+    public function index()
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->where('website', "高中個申")->where('status_time', "上架中")
+                        ->where('status', "發布")->findAll()
+        ];
+
+        return view('highApply/index', $data);
+    }
+
+    public function show($post_id)
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->find($post_id)
+        ];
+
+        return view('highApply/show', $data);
+    }
+
+    public function announce_1()
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->where('website', "高中個申")->where('status_time', "上架中")
+                        ->where('status', "發布")->where('category', "簡章訊息事項")->findAll()
+        ];
+
+        return view('highApply/announce_1', $data);
+    }
+
+    public function announce_2()
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->where('website', "高中個申")->where('status_time', "上架中")
+                        ->where('status', "發布")->where('category', "招生事務")->findAll()
+        ];
+
+        return view('highApply/announce_2', $data);
+    }
+
+    public function announce_3()
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->where('website', "高中個申")->where('status_time', "上架中")
+                        ->where('status', "發布")->where('category', "徵選資訊")->findAll()
+        ];
+
+        return view('highApply/announce_3', $data);
+    }
+
+    public function announce_4()
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->where('website', "高中個申")->where('status_time', "上架中")
+                        ->where('status', "發布")->where('category', "會議簡報")->findAll()
+        ];
+
+        return view('highApply/announce_4', $data);
+    }
+
+    public function announce_5()
+    {
+        $model = new Post();
+
+        $data = [
+            'posts' => $model->where('website', "高中個申")->where('status_time', "上架中")
+                        ->where('status', "發布")->where('category', "其他事項")->findAll()
+        ];
+
+        return view('highApply/announce_5', $data);
+    }
+
+    public function regulation()
+    {
+        return view('highApply/regulation');
+    }
+
+    public function schedule()
+    {
+        return view('highApply/schedule');
+    }
+
+    public function purchase_1()
+    {
+        return view('highApply/purchase_1');
+    }
+
+    public function purchase_2()
+    {
+        return view('highApply/purchase_2');
+    }
+
+    public function appform_1()
+    {
+        return view('highApply/appform_1');
+    }
+
+    public function appform_2()
+    {
+        return view('highApply/appform_2');
+    }
+
+    public function appform_3()
+    {
+        return view('highApply/appform_3');
+    }
+
+    public function appform_4()
+    {
+        return view('highApply/appform_4');
+    }
+
+    public function appform_5()
+    {
+        return view('highApply/appform_5');
+    }
+
+    public function appform_6()
+    {
+        return view('highApply/appform_6');
+    }
+
+    public function help_vulnerable()
+    {
+        return view('highApply/help_vulnerable');
+    }
+
+    public function statistic()
+    {
+        return view('highApply/statistic');
+    }
+
+    public function download_1()
+    {
+        return view('highApply/download_1');
+    }
+
+    public function download_2()
+    {
+        return view('highApply/download_2');
+    }
+
+    public function site_1()
+    {
+        return view('highApply/site_1');
+    }
+
+    public function site_2()
+    {
+        return view('highApply/site_2');
+    }
+
+    public function site_3()
+    {
+        return view('highApply/site_3');
+    }
+
+    public function history_statistics()
+    {
+        return view('highApply/history_statistics');
+    }
+
+    public function system_area_highschool()
+    {
+        return view('highApply/system_area_highschool');
+    }
+
+    public function system_area_college()
+    {
+        return view('highApply/system_area_college');
+    }
+
+    public function query()
+    {
+        return view('highApply/query');
+    }
+
+    public function online()
+    {
+        return view('highApply/online');
+    }
+
+    public function apply()
+    {
+        return view('highApply/apply');
+    }
+
+    public function freetelc()
+    {
+        return view('highApply/freetelc');
+    }
+
+    public function result()
+    {
+        return view('highApply/result');
+    }
+
+    public function dataupload()
+    {
+        return view('highApply/dataupload');
+    }
+
+    public function list()
+    {
+        return view('highApply/list');
+    }
+
+    public function rank()
+    {
+        return view('highApply/rank');
+    }
+
+    public function dispense()
+    {
+        return view('highApply/dispense');
+    }
+
+    public function abandon()
+    {
+        return view('highApply/abandon');
+    }
+}
---
+++ summer_preject_eric/app/Views/highApply/search.php
@@ -0,0 +1,73 @@
+<?= $this->extend('templates\highApply_temp') ?>
+
+<?= $this->section('head_info') ?>
+    <title>大學甄選入學委員會-簡章訊息事項</title>
+	<style>
+		#contentTable{
+			word-break:break-all;
+			word-wrap:break-all;
+		}
+	</style>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+	<div class="pt-2 pb-3 mx-lg-5 mx-md-3">
+        <div class="container d-flex flex-wrap justify-content-start">
+            <span class="fs-3">您搜尋的事項</span>
+        </div>
+    </div>
+	<div class="container border-top">
+		<table id="contentTable" class="table table-hover table-borderless">
+			<?php
+            use App\Models\Post;
+            $model = new Post();
+            $posts = $model->findAll();        
+			if(!empty($posts)) {
+				usort($posts, 'sort_by_update');
+				echo '
+					<thead>
+						<tr>
+							<th style="width: 20%"></th>
+							<th style="width: 20%"></th>
+							<th style="width: 60%"></th>
+						</tr>
+					</thead>
+					<tbody>
+				';
+				
+				foreach($posts as $posts_item) {
+					if($posts_item['website'] == "大學繁星" and $posts_item['status_time'] != "已下架" and $posts_item['status'] == "發布") {
+                                                                        
+                        foreach( $datas as $data_store){
+														
+                            if($posts_item['id'] == $data_store)
+                            {	
+                        echo '
+							<tr>
+								<td>'.substr($posts_item['update'], 0, 10).'</td>
+								<td>'.$posts_item['category'].'</td>
+								<td><a class="text-decoration-none" href="/UnivStar/show/'.$posts_item['id'].'">'.$posts_item['title'].'</a></td>
+							</tr>
+						';
+                            }                            
+                            
+                        }
+						
+					}
+                    
+				}
+                
+				echo '</tbody>';
+			}
+			?>
+		</table>
+	</div>
+
+	<?php
+	function sort_by_update($a, $b)
+	{
+		if($a['update'] == $b['update']) return 0;
+		return ($a['update'] > $b['update']) ? -1 : 1;
+	}
+	?>
+<?= $this->endSection() ?>
