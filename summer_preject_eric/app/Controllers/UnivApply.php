4424ec0a15e65a7306ec144e746eaec6199f5431
Mitch <wisenoone@gmail.com>
2022-08-31 15:49:54 +0800

    add and modify

--- summer_preject_eric/app/Controllers/UnivApply.php
+++ summer_preject_eric/app/Controllers/UnivApply.php
@@ -32,6 +32,7 @@ class UnivApply extends BaseController
         $data2 = [
             'posts' => $model2->find($post_id)
         ];
+        $user = $model->find($post_id);
         if($user["status_time"]=='已下架'||$user["status"]=='草稿')
         {         
         header("Location:http://localhost:8080/");//
