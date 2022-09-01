9cdeab1c76ca424b5fc6e8eaee3470a5aed04e56
leotang40 <leotang40@gmail.com>
2022-09-01 11:05:31 +0800

    add remind defore leave

--- summer_preject_eric/app/Controllers/PostController.php
+++ summer_preject_eric/app/Controllers/PostController.php
@@ -82,34 +82,34 @@ class PostController extends BaseController
     
     public function store()
     {   
-        $title = $this->request->getVar('title');        
-        $title = trim($title);
-        $title = preg_replace('/\s(?=)/', '', $title);
+        // $title = $this->request->getVar('title');        
+        // $title = trim($title);
+        // $title = preg_replace('/\s(?=)/', '', $title);
         $title = date('m-d-Y_h-i-s');
         $title = preg_replace('/\s(?=)/', '', $title);
         # 檢查檔案是否上傳成功        
-        if ($_FILES['file']['error'] === UPLOAD_ERR_OK){
-        /*
-          echo '檔案名稱: ' . $_FILES['my_file']['name'] . '<br/>';
-          echo '檔案類型: ' . $_FILES['my_file']['type'] . '<br/>';
-          echo '檔案大小: ' . ($_FILES['my_file']['size'] / 1024) . ' KB<br/>';
-          echo '暫存名稱: ' . $_FILES['my_file']['tmp_name'] . '<br/>';
-        */
-        # 檢查檔案是否已經存在
-        if (!(is_dir('File/'.$title)))//資料夾名稱不存在
-        {    $path = '../public/File/'.$title;
-             mkdir($path, 0777, false);//建立資料夾
-        } 
-        if (file_exists('File/'.$title.'/'.$_FILES['file']['name'])){
-            //echo '檔案已存在。<br/>';
-        } 
-        else {
-            $file = $_FILES['file']['tmp_name'];
-            $dest = 'File/'.$title.'/'.$_FILES['file']['name'];
-        
-            # 將檔案移至指定位置
-            move_uploaded_file($file, $dest);
-        }
+        if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+            /*
+            echo '檔案名稱: ' . $_FILES['my_file']['name'] . '<br/>';
+            echo '檔案類型: ' . $_FILES['my_file']['type'] . '<br/>';
+            echo '檔案大小: ' . ($_FILES['my_file']['size'] / 1024) . ' KB<br/>';
+            echo '暫存名稱: ' . $_FILES['my_file']['tmp_name'] . '<br/>';
+            */
+            # 檢查檔案是否已經存在
+            if (!(is_dir('File/'.$title))) {    //資料夾名稱不存在
+                $path = '../public/File/'.$title;
+                mkdir($path, 0777, false);  //建立資料夾
+            } 
+            if (file_exists('File/'.$title.'/'.$_FILES['file']['name'])){
+                //echo '檔案已存在。<br/>';
+            } 
+            else {
+                $file = $_FILES['file']['tmp_name'];
+                $dest = 'File/'.$title.'/'.$_FILES['file']['name'];
+            
+                # 將檔案移至指定位置
+                move_uploaded_file($file, $dest);
+            }
         }
         else {
             //echo '錯誤代碼：' . $_FILES['files']['error'] . '<br/>';
